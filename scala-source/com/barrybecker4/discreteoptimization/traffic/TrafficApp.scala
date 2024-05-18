9285c79fedd6bee90738297fae493148275cf5b0
Barry G Becker <barrybecker4@gmail.com>
2024-05-18 15:41:22 -0700

    Put TrafficApp at top level

--- scala-source/com/barrybecker4/discreteoptimization/traffic/TrafficApp.scala
+++ scala-source/com/barrybecker4/discreteoptimization/traffic/TrafficApp.scala
@@ -1,10 +1,11 @@
-package com.barrybecker4.discreteoptimization.traffic.viewer
+package com.barrybecker4.discreteoptimization.traffic
 
 import com.barrybecker4.discreteoptimization.common.graph.GraphTstUtil
 import com.barrybecker4.discreteoptimization.common.graph.visualization.{GraphStreamAdapter, GraphViewerFrame}
+import com.barrybecker4.discreteoptimization.traffic.viewer.TrafficViewerFrame
 
 
-object TrafficViewerApp extends App {
+object TrafficApp extends App {
 
   val frame = new TrafficViewerFrame()
 
